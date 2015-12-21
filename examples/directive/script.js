5414de74217c529bed6a2830dcde33647428d178
Roger Spielmann <rspielmann@snowflake.ch>
2015-12-21 17:14:42 +0100

    isolites the scope of the directive

--- examples/directive/script.js
+++ examples/directive/script.js
@@ -1,15 +1,16 @@
 var app = angular.module('myApp', []);
 
 app.controller('Controller', ['$scope', function($scope) {
-        $scope.customer = {
-            name: 'Naomi',
-            address: '1600 Amphitheatre'
-        };
-    }]);
+    $scope.naomi = {name: 'Naomi', address: '1600 Amphitheatre'};
+    $scope.igor = { name: 'Igor', address: '123 Somewhere'};
+}]);
 
 app.directive('myCustomer', function() {
         return {
             restrict: 'E',
+            scope: {
+                customerInfo: '=info'
+            },
             templateUrl: 'my-customer.html'
         };
     });
